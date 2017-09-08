ea0b7fc6b9e0da9100b17bab158fc36dbe13a53c
Sean Tauber <sean@Seans-MacBook-Air.local>
2017-09-08 08:51:19 -0500

    indent error

--- geodish.py
+++ geodish.py
@@ -112,37 +112,37 @@ class GeoDish:
 
 
 	def entity_sentiment_text(text, verbose=False):
-    """Detects entity sentiment in the provided text."""
- 	client = language_v1beta2.LanguageServiceClient()
+		"""Detects entity sentiment in the provided text."""
+		client = language_v1beta2.LanguageServiceClient()
 
-	if isinstance(text, six.binary_type):
-		text = text.decode('utf-8')
+		if isinstance(text, six.binary_type):
+			text = text.decode('utf-8')
 
-	document = types.Document(
-		content=text.encode('utf-8'),
-		type=enums.Document.Type.PLAIN_TEXT)
+		document = types.Document(
+			content=text.encode('utf-8'),
+			type=enums.Document.Type.PLAIN_TEXT)
 
-	# Pass in encoding type to get useful offsets in the response.
-	encoding = enums.EncodingType.UTF32
-	if sys.maxunicode == 65535:
-		encoding = enums.EncodingType.UTF16
+		# Pass in encoding type to get useful offsets in the response.
+		encoding = enums.EncodingType.UTF32
+		if sys.maxunicode == 65535:
+			encoding = enums.EncodingType.UTF16
 
-	result = client.analyze_entity_sentiment(document, encoding)
+		result = client.analyze_entity_sentiment(document, encoding)
 
-	if verbose:
-		for entity in result.entities:
-			print('Mentions: ')
-			print(u'Name: "{}"'.format(entity.name))
-			for mention in entity.mentions:
-				print(u'  Begin Offset : {}'.format(mention.text.begin_offset))
-				print(u'  Content : {}'.format(mention.text.content))
-				print(u'  Magnitude : {}'.format(mention.sentiment.magnitude))
-				print(u'  Sentiment : {}'.format(mention.sentiment.score))
-				print(u'  Type : {}'.format(mention.type))
-			print(u'Salience: {}'.format(entity.salience))
-			print(u'Sentiment: {}\n'.format(entity.sentiment))
+		if verbose:
+			for entity in result.entities:
+				print('Mentions: ')
+				print(u'Name: "{}"'.format(entity.name))
+				for mention in entity.mentions:
+					print(u'  Begin Offset : {}'.format(mention.text.begin_offset))
+					print(u'  Content : {}'.format(mention.text.content))
+					print(u'  Magnitude : {}'.format(mention.sentiment.magnitude))
+					print(u'  Sentiment : {}'.format(mention.sentiment.score))
+					print(u'  Type : {}'.format(mention.type))
+				print(u'Salience: {}'.format(entity.salience))
+				print(u'Sentiment: {}\n'.format(entity.sentiment))
 
-	return result
+		return result
 
 
 
