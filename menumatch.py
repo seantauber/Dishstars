f044c193d569bd31c0e97aa4002f88e457230100
Sean Tauber <sean@Seans-MacBook-Air.local>
2017-09-12 16:32:06 -0500

    bugfix

--- menumatch.py
+++ menumatch.py
@@ -7,6 +7,7 @@ def dishMatch(item, dishNames):
 	df = pd.DataFrame({'item': item, 'dish': dishNames})
 
 	ratio = lamba x: fuzz.ratio(x['item'], x['dish'])
-	df['fuzzRatio'] = df.apply(ratio)
+	df['fuzzRatio'] = df.apply(ratio, axis=1)
 
 	return df
+	
