84b498ebec7a780400b66800522f342310807668
Sean Tauber <sean@Seans-MacBook-Air.local>
2017-09-12 16:22:41 -0500

    menu match module

---
+++ menumatch.py
@@ -0,0 +1,12 @@
+from fuzzywuzzy import fuzz, process
+import pandas as pd
+
+def dishMatch(item, dishNames):
+	'''
+	'''
+	df = pd.DataFrame({'item': item, 'dish': dishNames})
+
+	ratio = lamba x: fuzz.ratio(x['item'], x['dish'])
+	df['fuzzRatio'] = df.apply(ratio)
+
+	return df
