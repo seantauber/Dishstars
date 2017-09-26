03e55cf5c5037f52dcf86f73165e32e9ba8c4d92
Sean Tauber <seanetauber@gmail.com>
2017-09-26 08:36:40 -0500

    working on recommender

--- dishrecommender.py
+++ dishrecommender.py
@@ -136,6 +136,7 @@ class DishRecommender:
 					results[item['dish']] = self.locationDishes[item['dish']]
 					similarDish = self.locationDishes[item['similarTo']]
 					similarTo = {'dish': similarDish['dish'], 'venueName': similarDish['venueName']}
+					results[item['dish']].update(similarTo)
 					totalItems += 1
 			else:
 				return results
