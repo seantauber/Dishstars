6d0524064a5100643529fdee045a1b7a48f38108
Sean Tauber <seanetauber@gmail.com>
2017-09-26 09:22:12 -0500

    working on recommender

--- dishrecommender.py
+++ dishrecommender.py
@@ -125,6 +125,7 @@ class DishRecommender:
 		
 		# sort them all by similarity
 		similar = sorted(similar, key=lambda x: x['similarity'])[::-1]
+		return similar
 		
 		# get the top n unique dishes and return all dish info
 		results = {}
