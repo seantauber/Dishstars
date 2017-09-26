cd46b774a0fbce65da21a36c4213fef1e1b36c63
Sean Tauber <seanetauber@gmail.com>
2017-09-26 00:02:55 -0500

    collaborative filter dish recommender

--- dishrecommender.py
+++ dishrecommender.py
@@ -1,21 +1,45 @@
-from geodish import GeoDish
+import numpy as np
+import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
+from scipy import sparse
 
 
 class DishRecommender:
 
-	def __init__(self):
+	def __init__(self, userlikedDishes, locationDishes):
 		'''
 		'''
-		self.geodish = GeoDish()
+		self.locationDishes = locationDishes
+		self.userlikedDishes = userlikedDishes
+
+		self.dishLookup = self.createDishLookup(self.self.locationDishes)
+		self.matrix = self.userByDishMatrix(self.self.locationDishes)
+		self.matrix = self.normalizeMatrix(self.mat)
+
+		self.userlikedVec = self.userlikedDishesVec(self.userLikedDishes)
+		
+		# Build the similarity matrix
+		self.similarityMatrix = calculateSimilarity(self.matrix)
+
+		self.dataNeighbors = self.getDataNeighbors(self.similarityMatrix)
 
 
-	def dishUserMatrix(self, locationId):
+
+
+
+	def createDishLookup(self, dishes):
+		'''
+		'''
+		lookup = {}
+		for key, val in dishes.items():
+			lookup[key] = val['dish']
+		return lookup
+
+	def userByDishMatrix(self, dishes):
 		'''
 		'''
 		dishUserLookup = {}
 		userDishLookup = {}
-
-		dishes = self.geodish.loadPopularDishes(locationId)
 
 		for dishKey, dishDetails in dishes.items():
 			for tip in dishDetails['dishTips']:
@@ -30,6 +54,94 @@ class DishRecommender:
 				else:
 					userDishLookup[tip['user']] = [dishKey]
 
-		return {'dishUser': dishUserLookup, 'userDish':userDishLookup}
+		ui = dict(zip(udm.keys(), range(len(udm.keys()))))
+		di = dict(zip(dum.keys(), range(len(dum.keys()))))
+		userLabels = sorted(ui, key=ui.get)
+		dishLabels = sorted(di, key=di.get)
+
+		mat = np.zeros((len(ui), len(di)))
+		for user, dishes in udm.items():
+			for dish in dishes:
+				mat[ui[user], di[dish]] = 1
+
+		mat = pd.DataFrame(mat, columns=dishLabels)
+
+		self.ui = ui
+		self.di = di
+
+		return mat
+
+	def userlikedDishesVec(self, dishes):
+		'''
+		'''
+		vec = np.zeros(len(self.di))
+		for dish in dishes:
+			vec[self.di[dish]] = 1
+
+		return vec
+
+
+	def normalizeMatrix(self, mat):
+		'''
+		'''
+		# Normalize the user vectors to unit vectors.
+
+		# magnitude = sqrt(x2 + y2 + z2 + ...)
+		magnitude = np.sqrt(np.square(mat).sum(axis=1))
+		# unitvector = (x / magnitude, y / magnitude, z / magnitude, ...)
+		mat = mat.divide(magnitude, axis='index')
+
+		return mat
+
+
+	def calculateSimilarity(self, data_items):
+		'''
+		Calculate the column-wise cosine similarity for a sparse
+		matrix. Return a new dataframe matrix with similarities.
+		'''
+		data_sparse = sparse.csr_matrix(data_items)
+		similarities = cosine_similarity(data_sparse.transpose())
+		sim = pd.DataFrame(data=similarities, index= data_items.columns, columns= data_items.columns)
+		return sim
+
+	
+	def getDataNeighbors(self, simMat):
+		'''
+		Construct a new dataframe with the 10 closest neighbours (most similar)
+		for each artist.
+		'''
+		data_neighbours = pd.DataFrame(index=data_matrix.columns, columns=range(1,11))
+		for i in xrange(0, len(data_matrix.columns)):
+			data_neighbours.ix[i,:10] = data_matrix.ix[0:,i].sort_values(ascending=False)[:10].index
+
+
+	def predictUserNeighborhood(self):
+		'''
+		Construct the neighbourhood from the most similar items to the
+		ones our user has already liked.
+		'''
+		mostSimilarToLikes = self.dataNeighbors.ix[self.userlikedDishes]
+		similarList = mostSimilarToLikes.values.tolist()
+		similarList = list(set([item for sublist in similarList for item in sublist]))
+		neighbourhood = self.similarityMatrix[similarList].ix[similarList]
+
+		# A user vector containing only the neighbourhood items and
+		# the known user likes.
+		userVector = self.userlikedVec.ix[similarList]
+
+		# Calculate the score.
+		score = neighbourhood.dot(userVector).div(neighbourhood.sum(axis=1))
+
+		# Drop the known likes.
+		score = score.drop(self.userLikedDishes)
+
+		return score
+
+
+
+
+
+
+
 
 
